0e6572c3b39a6a01ff60e35f33216278791c2265
Shashwat Shivam <46577918+sshivam1995@users.noreply.github.com>
2021-06-22 12:06:09 -0400

    Delete compute_derivative_xi.m

--- compute_derivative_xi.m
+++
@@ -1,5 +0,0 @@
-function xidot = compute_derivative_xi(xi,Z, theta, noise_on, fixed_prob)
-    dfdx = compute_dfdx_system(Z,theta,noise_on, fixed_prob);
-    dfdtheta = compute_dfdtheta(Z,theta, noise_on, fixed_prob);
-    xidot = dfdx * xi + dfdtheta;
-end
