245810a89154fa57622a0292723bce5403b8f054
Shashwat Shivam <46577918+sshivam1995@users.noreply.github.com>
2021-07-09 11:05:08 -0400

    Delete helper_functions directory

--- helper_functions/grad_J.m
+++
@@ -1,9 +0,0 @@
-function grad_j= grad_J(lambda, Z, optimal_input_old, t, dt, noise_on, fixed_prob, pars)
-    grad_j = zeros(1,2);
-    
-    for i = 1:length(t)
-        dfdtheta = compute_dfdtheta(Z(:,i),optimal_input_old, noise_on, fixed_prob, pars);
-        grad_j = grad_j + lambda(:,i)'*dfdtheta*dt;
-    end    
-end
-
